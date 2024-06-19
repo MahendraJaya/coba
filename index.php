7bee604c3ce63207d3f7ec9f8244e9e62561e1af
MahendraJaya <hendraputra0645@gmail.com>
2024-06-19 21:34:13 +0700

    done add bold

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     <h1>TES</h1>
     <h5>TES</h5>
     <h6>TES</h6>
+    <b>HABIS NAMBAH BOLD</b>
+    <p>PPPPPPP</p>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, quaerat quod, atque sunt repellat molestias id possimus numquam culpa quis illum architecto, optio ab eos error itaque dolor repudiandae magnam?
     Nisi, quae numquam explicabo impedit eos maxime. Sint maxime, ea laborum illo inventore atque corporis eos non officia magnam tempora ipsa consequuntur similique, at sequi! Quibusdam a natus aspernatur eligendi.
     Dignissimos quidem id porro expedita aut consectetur, perspiciatis quia vel incidunt minus reprehenderit quasi commodi veritatis inventore veniam eos culpa laborum molestiae atque nihil ullam! Unde vel natus eum eaque.
